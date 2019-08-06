98651c58477ef55e9e1842febf57ca92b5883ee7
Barbara Muesing <muesingb@gmail.com>
2019-08-06 18:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+//returns "Happy holidays, ${name}!"
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+//returns "Happy ${holiday}, ${name}!"
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
